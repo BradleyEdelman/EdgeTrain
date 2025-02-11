7ca642548e6319a9263d079a8c622d900877ea7c
BradleyEdelman <bjedelma@gmail.com>
2025-02-11 15:45:33 +0100

    CI issue fix?

--- edgetrain/__init__.py
+++ edgetrain/__init__.py
@@ -1,3 +1,12 @@
+from .adjust_train_parameters import adjust_training_parameters
+from .calculate_priorities import define_priorities
+from .calculate_scores import compute_scores, normalize_scores
+from .create_model import check_sparsity, create_model_tf
+from .dynamic_train import dynamic_train
+from .edgetrain_folder import get_edgetrain_folder
+from .resource_monitor import log_usage_once, sys_resources
+from .train_visualize import log_train_time, log_usage_plot, training_history_plot
+
 __all__ = [
     "adjust_training_parameters",
     "define_priorities",
